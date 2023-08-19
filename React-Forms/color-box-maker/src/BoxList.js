6bbfd61e5e9fbe8762abdc0141137be38fd96b18
Nichole Murphy <nicholejmurphy@gmail.com>
2023-08-19 09:25:32 -0400

    First Commit

---
+++ React-Forms/color-box-maker/src/BoxList.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import NewBoxForm from "./NewBoxForm";
+import Box from "./Box";
+import { v4 as uuid } from "uuid";
+
+function BoxList() {}
+
+export default BoxList;
---
+++ React-Forms/color-box-maker/src/NewBoxForm.js
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+
+const NewBoxForm = () => {};
+
+export default NewBoxForm;
