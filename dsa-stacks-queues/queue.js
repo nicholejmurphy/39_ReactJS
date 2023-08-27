6e8842487816ac4e2680b758d7139fad0f300ab1
Nichole Murphy <nicholejmurphy@gmail.com>
2023-08-27 12:09:43 -0400

    Completed queue exercise

---
+++ dsa-stacks-queues/queue.js
@@ -0,0 +1,61 @@
+/** Node: node for a queue. */
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+/** Queue: chained-together nodes where you can
+ *  remove from the front or add to the back. */
+
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /** enqueue(val): add new value to end of the queue. Returns undefined. */
+
+  enqueue(val) {
+    const newVal = new Node(val);
+
+    if (this.size === 0) {
+      this.first = newVal;
+      this.last = newVal;
+    } else {
+      this.last.next = newVal;
+      this.last = newVal;
+    }
+    this.size += 1;
+  }
+
+  /** dequeue(): remove the node from the start of the queue
+   * and return its value. Should throw an error if the queue is empty. */
+
+  dequeue() {
+    if (this.size === 0) {
+      throw new Error("Queue is empty, no item to remove.");
+    }
+    const removed = this.first;
+    this.first = this.first.next;
+    this.size -= 1;
+    return removed.val;
+  }
+
+  /** peek(): return the value of the first node in the queue. */
+
+  peek() {
+    return this.first.val;
+  }
+
+  /** isEmpty(): return true if the queue is empty, otherwise false */
+
+  isEmpty() {
+    return this.size === 0;
+  }
+}
+
+module.exports = Queue;
